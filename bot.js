fd8eaa6336550fa98c9f474b7097d3bde71be6a3
Glitch (rudego) <none>
2018-05-05 11:31:06 +0000

    👾💽 Checkpoint ./cmds/pomoc.js:835512/114 ./bot.js:835512/1414 ./config.json:835512/23 ./watch.json:835512/9

--- bot.js
+++ bot.js
@@ -16,7 +16,6 @@ const botSettings = require('./config.json');
 const fs = require("fs");
 
 bot.commands = new Discord.Collection();
-const profiles = require('./profiles.json');
 
 
 fs.readdir('./cmds/', (err, files) => {
@@ -41,7 +40,7 @@ bot.on('ready', () => {
     console.log(`${bot.user.username} is ready for deployment!`);
     console.log(`ID: ${bot.user.id}, Server Count:${bot.guilds.size}`);
     
-    bot.user.setActivity(`oblizywanie łapek i czekanie na rhelp`)
+    bot.user.setActivity(`oblizywanie łapek || rpomoc`)
     //bot.setInterval(() => {
         //bot.user.setActivity(`to ${bot.users.size} users || d.help`, { type: "LISTENING"});
     //}, 5000)
@@ -54,48 +53,7 @@ bot.on('message', async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    let freeRanks = JSON.parse(fs.readFileSync("./ranks.json", "utf8"));
-    if(!freeRanks[message.guild.id]){
-        freeRanks[message.guild.id] = {
-            ranksID: 0
-        };
-    }
-
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-      prefixes[message.guild.id] = {
-        prefixes: botSettings.prefix
-      };
-    }
-
-    let xpAdd = Math.floor(Math.random() * 10) + 1;
-
-    if(!profiles[message.author.id]){
-        profiles[message.author.id] = {
-            xp: 0,
-            level: 1,
-            Dollars: 0
-        };
-    }
-
-    let curXp = profiles[message.author.id].xp
-    let curLvl = profiles[message.author.id].level
-    profiles[message.author.id].xp = curXp + xpAdd;
-    let nxtLvl = 5 * (curLvl ^ 2) + 50 * curLvl + 100
-
-    if(nxtLvl <= profiles[message.author.id].xp){
-        profiles[message.author.id].level = curLvl + 1
-        let lvlup = new Discord.RichEmbed()
-            .setTitle("You leveled up!")
-            .setDescription(`You have just reached level ${curLvl + 1}!`)
-
-        message.channel.send(lvlup)
-    }
-    fs.writeFile("./profiles.json", JSON.stringify(profiles), (err) => {
-        if(err) console.log(err)
-    })
-
-    let prefix = prefixes[message.guild.id].prefixes;
+    let prefix = botSettings.prefix
     let messageArray = message.content.split(/\s+/g);
     let command = messageArray[0]
     let args = messageArray.slice(1);
