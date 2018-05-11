efefc325d6b5fb64c733b6d1554a6acb52c235fd
Glitch (rudego) <none>
2018-05-11 21:22:34 +0000

    🚔✨ Checkpoint ./cmds/dodaj-punkty.js:835512/716 ./watch.json:835512/10

--- cmds/dodaj-punkty.js
+++ cmds/dodaj-punkty.js
@@ -1,15 +1,15 @@
 const Discord = module.require("discord.js");
 let profiles = require("../profile.json");
+const fs = require("fs");
 
 exports.run = (client, message, args) => {
   
-    let użyt = args[0]
+    let punktyDodaj = args[0]
+    if(!punktyDodaj) return ("musisz podać ilość punktów.")
+  
+    let użyt = args[1]
     użyt = message.mentions.users.first()
     if(!użyt) return message.reply("nie podałeś użytkownika.")
-  
-    let punktyDodaj = args[1]
-    if(!isNaN(punktyDodaj)) return message.reply("podana wartość nie jest numerem.")
-    if(!punktyDodaj) return ("musisz podać ilość punktów.")
   
     let zesSerw = message.guild.roles.find(`name`,"Zespół Serwera")
     if(message.member.roles.has(zesSerw)) return message.reply("nie masz permisji żeby tą komendę wykonać.") 
@@ -21,6 +21,12 @@ exports.run = (client, message, args) => {
     }
 
     let punkty = profiles[użyt.id].punkty
+    let nowePunkty = punkty + punktyDodaj
+    
+    profiles[użyt.id].Dollars = nowePunkty
+        fs.writeFile("../profile.json", JSON.stringify(profiles), (err) => {
+            if(err) console.log(err)
+        })
 }
 
 module.exports.help = {
